41601d0345bb90506a75ca212581e16a3a75fe09
Mikael Hadler <mikaelhadler@gmail.com>
2020-06-30 18:32:30 -0300

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <form name="contact" method="POST" data-netlify="true">
+    <form name="contact" data-netlify="true">
       <p>
         <label>Your Name: <input type="text" name="name" /></label>
       </p>
