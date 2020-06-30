fae9bc2ec254b76057ed16d7b5433455ad1b1aaf
Mikael Hadler <mikaelhadler@gmail.com>
2020-06-30 18:39:58 -0300

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <form name="contact" data-netlify="true" data-netlify-honeypot="bot-field">
+    <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
       <p>
         <label>Your Name: <input type="text" name="name" /></label>
       </p>
